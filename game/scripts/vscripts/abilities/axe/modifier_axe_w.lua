462d88bd0bbbe0b4096436406f9139c9c3554137
Evgeny <hekabekahe@gmail.com>
2019-03-11 13:57:50 +0200

    Axe W

--- game/scripts/vscripts/abilities/axe/modifier_axe_w.lua
+++ game/scripts/vscripts/abilities/axe/modifier_axe_w.lua
@@ -18,13 +18,34 @@ function self:GetStatusEffectName()
 end
 
 function self:AllowAbilityEffect(source, ability, data)
-    local caster = self:GetCaster():GetParentEntity()
+    local hero = self:GetParent():GetParentEntity()
     
     if data and data.damage then
         local damage = data.damage.count
-        if data.damage.count > 0 then
-            caster:AddNewModifier(caster, caster:FindAbility("axe_w"), "modifier_axe_w_dmg", { duration = 2.5 }):GetDmg(source, damage, data.damage.type, ability)
-            self.damageReceived = damage
+        if damage > 0 then
+            local invulDuration = 0.5 + (damage / 2)
+            local modifier = hero:AddNewModifier(hero, hero:FindAbility("axe_w"), "modifier_axe_w_dmg", { duration = math.max(2.5, invulDuration)})
+            modifier.damageSource = source
+            modifier.isPhysical = data.damage.type
+            modifier.damage = damage
+            modifier.sourceAbility = ability
+
+            hero:EmitSound("Arena.Axe.ProcW")
+            hero:AddNewModifier(hero, hero:FindAbility("axe_w"), "modifier_axe_w_bonus", { duration = invulDuration })
+
+            FX("particles/axe_w/axe_w_explosion.vpcf", PATTACH_WORLDORIGIN, hero, {
+                cp0 = self:GetParent():GetAbsOrigin(),
+                cp1 = Vector(300, 1, 1),
+                cp0 = self:GetParent():GetAbsOrigin(),
+                release = true
+            })
+
+            local mod = hero:FindModifier("modifier_axe_counter")
+            if mod:GetStackCount() < 3 and not hero:FindModifier("modifier_axe_rage") then
+                mod:IncrementStackCount()
+            end
+
+            self.activated = true
             self:Destroy()
 
             return false
@@ -34,26 +55,7 @@ end
 
 if IsServer() then
     function self:OnDestroy()
-        local ability = self:GetAbility()
-        local hero = self:GetCaster():GetParentEntity()
-
-        if self.damageReceived then
-            local duration = 0.5 + (self.damageReceived / 2)
-            self:GetParent():GetParentEntity():EmitSound("Arena.Axe.ProcW")
-            hero:AddNewModifier(hero, hero:FindAbility("axe_w"), "modifier_axe_w_bonus", { duration = duration })
-
-            FX("particles/axe_w/axe_w_explosion.vpcf", PATTACH_WORLDORIGIN, hero, {
-                cp0 = self:GetParent():GetAbsOrigin(),
-                cp1 = Vector(300, 1, 1),
-                cp0 = self:GetParent():GetAbsOrigin(),
-                release = true
-            })
-            
-            local mod = hero:FindModifier("modifier_axe_counter")
-            if mod:GetStackCount() < 3 and not hero:FindModifier("modifier_axe_rage") then
-                mod:IncrementStackCount()
-            end
-        else
+        if not self.activated then
             self:GetParent():GetParentEntity():EmitSound("Arena.Axe.EndW")
         end
     end
